bb2a3c366b07cccb9fd6079353c953d6171a1cd8
David Smiles <ugberodavid@gmail.com>
2021-09-09 16:52:27 +0100

    improve log output on logger middleware

--- payment/middlewares/logger.js
+++ payment/middlewares/logger.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('HOST')}${req.originalUrl}: ${moment().format()}`)
+    console.log(`${moment().format()}: ${res.statusCode} /${req.method} ${req.protocol}://${req.get('HOST')}${req.originalUrl}`)
     next()
 }
 
-module.exports = logger
+module.exports = logger 
